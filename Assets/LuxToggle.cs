c26a14e3d038c26313d074dbbb6091a92213ee2b
Caroline Sih <32976407+carolinesih@users.noreply.github.com>
2018-04-15 12:17:15 -0700

    first commit

---
+++ Assets/LuxToggle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LuxToggle : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    void Update()
+    {
+        foreach(Transform child in transform)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //GetComponent<Renderer>().enabled = !GetComponent<Renderer>().enabled;
+                if (child.gameObject.activeSelf)
+                {
+                    child.gameObject.SetActive(false);
+                }
+                else
+                {
+                    child.gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+}
